bf2db215c51a8d9153c077559d56a2cd717dd48a
nonydev27 <karldjansi123@gmail.com>
2026-01-04 04:46:00 +0000

    done styling UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+import { useState } from "react";
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <div className=" font-sans">
-        <div className="text-4xl">00</div>
+      <div className=" font-sans flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="text-9xl">00</div>
 
-        <button className="button bg-blue-600 p-4 w-15 ">start</button>
+        <div className="mt-5 flex gap-4">
+          <button className="button uppercase border px-9 flex justify-center bg-blue-600 p-4 w-15 hover:bg-white hover:text-blue-500 hover:outline hover:outline-blue-500 transition ease-in-out duration-300 hover:cursor-pointer hover:transform hover:scale-115">
+            start
+          </button>
 
-        <button className="button bg-blue-600 p-4 w-15 ">stop</button>
+          <button className="button uppercase border px-9 flex justify-center bg-red-600 p-4 w-15 hover:bg-white hover:text-red-500 hover:outline hover:outline-red-500 transition ease-in-out duration-300 hover:cursor-pointer hover:transform hover:scale-115">
+            stop
+          </button>
 
-        <button className="button bg-blue-600 p-4 w-15 ">reset</button>
+          <button className="button uppercase border px-9 flex justify-center bg-green-600 p-4 w-15 hover:bg-white hover:text-green-500 hover:outline hover:outline-green-500 transition ease-in-out duration-300 hover:cursor-pointer hover:transform hover:scale-115">
+            reset
+          </button>
+        </div>
       </div>
     </>
   );
