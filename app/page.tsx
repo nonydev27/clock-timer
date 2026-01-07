0dbdd56eb50678380c9a9eaf1e9c778e06aaba91
Elorm <agbekoelorm32@gnail.com>
2026-01-07 21:10:01 +0000

    done setting up spans

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <span className="text-9xl">00:</span>{" "}
           <span className="text-9xl">00</span>
         </div>
-        <div className="mt-5 flex gap-4">
+        <div className="mt-5 flex gap-">
           <button className="button uppercase border px-9 flex justify-center bg-blue-600 p-4 w-15 hover:bg-white hover:text-blue-500 hover:outline hover:outline-blue-500 transition ease-in-out duration-300 hover:cursor-pointer hover:transform hover:scale-115">
             start
           </button>
