d8e00e88f5802d89320d9cc2ff6e56d8667e896f
nonydev27 <karldjansi123@gmail.com>
2026-01-08 10:33:56 +0000

    Add initial structure for the app's main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 export default function Home() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [backgroundColor, setBackgroundColor] = useState("white");
 
   return (
